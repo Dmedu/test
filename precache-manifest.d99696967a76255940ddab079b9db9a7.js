adb4f525cc1b79f5c774996719aeec4882a6a10b
Alex <610558983@qq.com>
2020-05-03 15:44:50 +0800

    云控制page

---
+++ precache-manifest.d99696967a76255940ddab079b9db9a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d863eafa9d085ecb2b94b6fb4f4055",
+    "url": "./index.html"
+  },
+  {
+    "revision": "507165e580fb18bd6110",
+    "url": "./static/css/2.6468e9fc.chunk.css"
+  },
+  {
+    "revision": "eed7b2155bd68e1e83bf",
+    "url": "./static/css/main.34c7aa79.chunk.css"
+  },
+  {
+    "revision": "507165e580fb18bd6110",
+    "url": "./static/js/2.c192e4ae.chunk.js"
+  },
+  {
+    "revision": "1ca72f787716591d45b9c2d398f9fbdf",
+    "url": "./static/js/2.c192e4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eed7b2155bd68e1e83bf",
+    "url": "./static/js/main.f3cde984.chunk.js"
+  },
+  {
+    "revision": "f5bf5cf31714b25ec08c",
+    "url": "./static/js/runtime-main.190b65fb.js"
+  },
+  {
+    "revision": "1a80a0a4f0fb36c0051a4dcb5482abd2",
+    "url": "./static/media/404.1a80a0a4.svg"
+  },
+  {
+    "revision": "a66a9a6631de4179a441dfe972f48ac9",
+    "url": "./static/media/login_bg.a66a9a66.svg"
+  },
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "./static/media/logo.b29324c4.svg"
+  },
+  {
+    "revision": "062c880b4a3ee9f329942f2d7487999d",
+    "url": "./static/media/name.062c880b.svg"
+  }
+]);
